be3a9fe35c7f2b3821d705fdb9684e5d4098d5e5
Akshay2047A <omakshayreddy2047@gmail.com>
2026-04-28 01:21:19 +0530

    v3

--- command-center/list_models.py
+++ command-center/list_models.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 import os
 import google.generativeai as genai
 from dotenv import load_dotenv
--- command-center/test_gemini.py
+++ command-center/test_gemini.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 import app
 import base64
 import os
